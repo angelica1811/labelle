a4dd508c6c1dad3e91504430b8fa25a9a80a3d90
angelica1811 <77701214+angelica1811@users.noreply.github.com>
2023-03-30 14:15:40 -0600

    page content

---
+++ page-about.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+        <img src="" alt="">
+
+        <h1>About Labelle Tile and Stone</h1>
+
+        <div>
+            <div>
+                <h2>Lorem Ipsum</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga eligendi nemo itaque illum ipsam explicabo optio ratione libero modi distinctio maxime nisi sint alias consequuntur dolore quo, repellendus accusantium tempore?
+                </p>
+            </div>
+            <div>
+                <img src="" alt="">
+            </div>
+        </div>
+
+        <div>
+            <div>
+                <h2>Lorem Ipsum</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil sequi eum tenetur mollitia, numquam fuga repellendus iste voluptatum porro eius officia necessitatibus nesciunt cupiditate fugit? Animi soluta tempora nulla fuga.
+                </p>
+                <a href="#">Button</a>
+            </div>
+            <div>
+                <img src="" alt="">
+                <img src="" alt="">
+            </div>
+        </div>
+    </section>
+</body>
+</html>
---
+++ page-services.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+        <div>
+            <div>
+                <h1>Lorem Ipsum</h1>
+                <h2>Lorem Ipsum</h2>
+                <h2>Lorem Ipsum</h2>
+            </div>
+
+            <div>
+                <h2>Lorem Ipsum</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita fugiat iste hic, voluptates perferendis temporibus eligendi minus obcaecati ducimus natus nesciunt accusamus modi inventore commodi veritatis blanditiis dolorum molestias culpa!
+                </p>
+                <a href="#">Button</a>
+            </div>
+        </div>
+
+        <div>
+            <h2>Services</h2>
+
+            <div>
+                <img src="" alt="">
+                <h3>Lorem Ipsum</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore eligendi quis libero incidunt error, vero officia quasi doloremque aperiam porro tempora nobis qui numquam exercitationem? Consequuntur hic qui nulla eos?
+                </p>
+            </div>
+
+            <div>
+                <img src="" alt="">
+                <h3>Lorem Ipsum</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore eligendi quis libero incidunt error, vero officia quasi doloremque aperiam porro tempora nobis qui numquam exercitationem? Consequuntur hic qui nulla eos?
+                </p>
+            </div>
+
+            <div>
+                <img src="" alt="">
+                <h3>Lorem Ipsum</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore eligendi quis libero incidunt error, vero officia quasi doloremque aperiam porro tempora nobis qui numquam exercitationem? Consequuntur hic qui nulla eos?
+                </p>
+            </div>
+        </div>
+
+        <div>
+            <div>
+                <h2>Lorem Ipsum</h2>
+                <div>
+                    <a href="#">Button</a>
+                    <a href="#">Button</a>
+                    <a href="#">Button</a>
+                    <a href="#">Button</a>
+                    <a href="#">Button</a>
+                </div>
+            </div>
+
+            <div>
+                <h2>Lorem Ipsum</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat voluptatum earum iure pariatur in rem quas quisquam! Commodi dolorem delectus dignissimos, repellendus voluptates sit voluptatum accusantium, rerum incidunt consectetur voluptas.
+                </p>
+                <a href="#">Button</a>
+            </div>
+
+            <div>
+                <div>
+                    <img src="" alt="">
+                </div>
+
+                <div>
+                    <img src="" alt="">
+                    <img src="" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
