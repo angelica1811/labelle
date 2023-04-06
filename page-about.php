0cf8aecc43c1c527a6b3a9815aa70a9d9c40f07e
angelica1811 <77701214+angelica1811@users.noreply.github.com>
2023-04-05 23:26:29 -0600

    about page html

---
+++ page-about.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package labelle
+ */
+
+get_header();
+?>
+
+<body>
+    <section class="about-us-section">
+        <div class="hero-banner">
+            <img src="" alt="">
+        </div>
+
+        <div>
+            <h1>About Us</h1>
+
+            <div class="about-us">
+                <div class="about-company">
+                    <div>
+                        <h2>Labelle Tile and Stone</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit porro distinctio, corrupti
+                            eum
+                            laborum esse ipsum, libero ipsam minus modi itaque rem saepe. Rem, recusandae dignissimos
+                            officiis soluta cum laboriosam.
+                        </p>
+                        <a href="#">Services</a>
+                    </div>
+
+                    <div>
+                        <img src="" alt="">
+                    </div>
+                </div>
+
+                <div class="about-alain">
+                    <div>
+                        <h2>Meet Alain Labelle</h2>
+                        <p>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat at a amet accusantium
+                            minus. Totam ipsam omnis culpa possimus qui, cumque dignissimos placeat beatae, voluptas
+                            voluptatibus doloremque optio, nulla ipsum!
+                        </p>
+                        <a href="#">Contact</a>
+                    </div>
+
+                    <div>
+                        <img src="" alt="">
+                        <img src="" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+<?php get_footer(); ?>
---
+++ page-contact.php
@@ -0,0 +1,6 @@
+<?php get_header(); ?>
+
+<h1>Contact</h1>
+<p>sdfsfsdf</p>
+
+<?php get_footer(); ?>
