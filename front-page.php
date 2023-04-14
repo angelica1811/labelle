7e6b0b589a2292b53cf82d40d9632d67eb41b5c2
angelica1811 <77701214+angelica1811@users.noreply.github.com>
2023-04-14 01:20:29 -0600

    content and styling

---
+++ front-page.php
@@ -0,0 +1,145 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package amybollana
+ */
+
+get_header();
+?>
+
+<body>
+    <section class="top">
+        <h1>Welcome to Labelle Tile & Stone</h1>
+        <div class="top-about">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae dolor beatae laborum, nobis
+                possimus
+                nesciunt nihil fugiat fugit quo quam itaque iusto dolorum vitae accusantium, nulla mollitia. Fugiat,
+                odit
+                necessitatibus.
+            </p>
+            <button class="btn">Services</button>
+        </div>
+        <div>
+            <img src="" alt="">
+        </div>
+    </section>
+
+    <section class="about">
+        <div class="about-alain">
+            <h2>Meet Alain Labelle</h2>
+            <img src="" alt="">
+            <img src="" alt="">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, sequi dignissimos laborum
+                possimus vero commodi iure nostrum consectetur est aliquid eos nam facere exercitationem quia adipisci
+                sit quidem inventore? Vitae.
+            </p>
+            <div>
+                <a href="#">Contact</a>
+            </div>
+        </div>
+
+        <div class="about-business">
+            <div>
+                <h3>About Labelle Tile & Stone</h3>
+            </div>
+
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum, beatae laborum! Modi accusantium,
+                deleniti, possimus minima veniam voluptates perferendis officiis quaerat ab fuga labore sunt expedita
+                esse, dolorem et ea?
+            </p>
+        </div>
+    </section>
+
+    <section class="services">
+        <div>
+            <h2>Services</h2>
+            <h5>Lorem Ipsum</h5>
+            <h5>Lorem Ipsum</h5>
+            <h5>Lorem Ipsum</h5>
+        </div>
+
+        <div class="why-labelle">
+            <h2>Why Labelle Tile and Stone?</h2>
+            <div>
+                <div>
+                    <img src="" alt="">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere est inventore natus earum vitae
+                        molestiae nam ducimus, autem aspernatur, veritatis consequuntur sed rem exercitationem itaque
+                        provident. Vel sunt optio modi.
+                    </p>
+                </div>
+
+                <div>
+                    <img src="" alt="">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere est inventore natus earum vitae
+                        molestiae nam ducimus, autem aspernatur, veritatis consequuntur sed rem exercitationem itaque
+                        provident. Vel sunt optio modi.
+                    </p>
+                </div>
+
+                <div>
+                    <img src="" alt="">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere est inventore natus earum vitae
+                        molestiae nam ducimus, autem aspernatur, veritatis consequuntur sed rem exercitationem itaque
+                        provident. Vel sunt optio modi.
+                    </p>
+                </div>
+            </div>
+
+            <div class="installation">
+                <h2>Installation Services</h2>
+                <div>
+                    <a href="#">Handmade Tiles</a>
+                    <a href="#">Ledgestone</a>
+                    <a href="#">Natural Stone</a>
+                    <a href="#">Porcelain</a>
+                    <a href="#">Terrazzo</a>
+                    <a href="#">Cement Tile</a>
+                    <a href="#">Glass</a>
+                    <a href="#">Mosaics</a>
+                    <a href="#">Ceramic</a>
+                </div>
+
+                <div>
+                    <img src="" alt="">
+                    <div>
+                        <img src="" alt="">
+                        <img src="" alt="">
+                    </div>
+                </div>
+
+                <div>
+                    <h3>Handmade Tiles</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed eaque inventore itaque dolorem,
+                        exercitationem ex saepe ad at iure tempora. Eligendi facilis architecto sed ducimus nam at totam
+                        minus impedit!</p>
+                    <a href="#">Contact</a>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+
+    <section>
+        <h2>Contact Us</h2>
+        <!-- insert form -->
+    </section>
+
+</body>
+
+</html>
--- js/navigation.js
+++ js/navigation.js
@@ -1,99 +1,122 @@
-/**
- * File navigation.js.
- *
- * Handles toggling the navigation menu for small screens and enables TAB key
- * navigation support for dropdown menus.
- */
-( function() {
-	const siteNavigation = document.getElementById( 'site-navigation' );
+// /**
+//  * File navigation.js.
+//  *
+//  * Handles toggling the navigation menu for small screens and enables TAB key
+//  * navigation support for dropdown menus.
+//  */
+(function () {
+	console.log('js is linked')
+	document.addEventListener("DOMContentLoaded", () => {
+		const hamburger = document.querySelector(".menu-toggle");
+		const navMenu = document.querySelector(".nav-menu");
 
-	// Return early if the navigation doesn't exist.
-	if ( ! siteNavigation ) {
-		return;
-	}
+		hamburger.addEventListener("click", () => {
+			hamburger.classList.toggle("active");
+			navMenu.classList.toggle("active");
+		});
 
-	const button = siteNavigation.getElementsByTagName( 'button' )[ 0 ];
+		document.querySelectorAll(".nav-menu > .menu-item > .menu-link").forEach(n => n.addEventListener("click", () => {
+			hamburger.classList.remove("active");
+			navMenu.classList.remove("active");
+		}));
+	});
+})();
 
-	// Return early if the button doesn't exist.
-	if ( 'undefined' === typeof button ) {
-		return;
-	}
 
-	const menu = siteNavigation.getElementsByTagName( 'ul' )[ 0 ];
 
-	// Hide menu toggle button if menu is empty and return early.
-	if ( 'undefined' === typeof menu ) {
-		button.style.display = 'none';
-		return;
-	}
 
-	if ( ! menu.classList.contains( 'nav-menu' ) ) {
-		menu.classList.add( 'nav-menu' );
-	}
 
-	// Toggle the .toggled class and the aria-expanded value each time the button is clicked.
-	button.addEventListener( 'click', function() {
-		siteNavigation.classList.toggle( 'toggled' );
+// (function () {
+// 	console.log('js is linked')
+// 	const siteNavigation = document.getElementById('site-navigation');
 
-		if ( button.getAttribute( 'aria-expanded' ) === 'true' ) {
-			button.setAttribute( 'aria-expanded', 'false' );
-		} else {
-			button.setAttribute( 'aria-expanded', 'true' );
-		}
-	} );
+// 	// Return early if the navigation doesn't exist.
+// 	if (!siteNavigation) {
+// 		return;
+// 	}
 
-	// Remove the .toggled class and set aria-expanded to false when the user clicks outside the navigation.
-	document.addEventListener( 'click', function( event ) {
-		const isClickInside = siteNavigation.contains( event.target );
+// 	const button = siteNavigation.getElementsByTagName('button')[0];
 
-		if ( ! isClickInside ) {
-			siteNavigation.classList.remove( 'toggled' );
-			button.setAttribute( 'aria-expanded', 'false' );
-		}
-	} );
+// 	// Return early if the button doesn't exist.
+// 	if ('undefined' === typeof button) {
+// 		return;
+// 	}
 
-	// Get all the link elements within the menu.
-	const links = menu.getElementsByTagName( 'a' );
+// 	const menu = siteNavigation.getElementsByTagName('ul')[0];
 
-	// Get all the link elements with children within the menu.
-	const linksWithChildren = menu.querySelectorAll( '.menu-item-has-children > a, .page_item_has_children > a' );
+// 	// Hide menu toggle button if menu is empty and return early.
+// 	if ('undefined' === typeof menu) {
+// 		button.style.display = 'none';
+// 		return;
+// 	}
 
-	// Toggle focus each time a menu link is focused or blurred.
-	for ( const link of links ) {
-		link.addEventListener( 'focus', toggleFocus, true );
-		link.addEventListener( 'blur', toggleFocus, true );
-	}
+// 	if (!menu.classList.contains('nav-menu')) {
+// 		menu.classList.add('nav-menu');
+// 	}
 
-	// Toggle focus each time a menu link with children receive a touch event.
-	for ( const link of linksWithChildren ) {
-		link.addEventListener( 'touchstart', toggleFocus, false );
-	}
+// 	// Toggle the .toggled class and the aria-expanded value each time the button is clicked.
+// 	button.addEventListener('click', function () {
+// 		siteNavigation.classList.toggle('toggled');
 
-	/**
-	 * Sets or removes .focus class on an element.
-	 */
-	function toggleFocus() {
-		if ( event.type === 'focus' || event.type === 'blur' ) {
-			let self = this;
-			// Move up through the ancestors of the current link until we hit .nav-menu.
-			while ( ! self.classList.contains( 'nav-menu' ) ) {
-				// On li elements toggle the class .focus.
-				if ( 'li' === self.tagName.toLowerCase() ) {
-					self.classList.toggle( 'focus' );
-				}
-				self = self.parentNode;
-			}
-		}
+// 		if (button.getAttribute('aria-expanded') === 'true') {
+// 			button.setAttribute('aria-expanded', 'false');
+// 		} else {
+// 			button.setAttribute('aria-expanded', 'true');
+// 		}
+// 	});
 
-		if ( event.type === 'touchstart' ) {
-			const menuItem = this.parentNode;
-			event.preventDefault();
-			for ( const link of menuItem.parentNode.children ) {
-				if ( menuItem !== link ) {
-					link.classList.remove( 'focus' );
-				}
-			}
-			menuItem.classList.toggle( 'focus' );
-		}
-	}
-}() );
+// 	// Remove the .toggled class and set aria-expanded to false when the user clicks outside the navigation.
+// 	document.addEventListener('click', function (event) {
+// 		const isClickInside = siteNavigation.contains(event.target);
+
+// 		if (!isClickInside) {
+// 			siteNavigation.classList.remove('toggled');
+// 			button.setAttribute('aria-expanded', 'false');
+// 		}
+// 	});
+
+// 	// Get all the link elements within the menu.
+// 	const links = menu.getElementsByTagName('a');
+
+// 	// Get all the link elements with children within the menu.
+// 	const linksWithChildren = menu.querySelectorAll('.menu-item-has-children > a, .page_item_has_children > a');
+
+// 	// Toggle focus each time a menu link is focused or blurred.
+// 	for (const link of links) {
+// 		link.addEventListener('focus', toggleFocus, true);
+// 		link.addEventListener('blur', toggleFocus, true);
+// 	}
+
+// 	// Toggle focus each time a menu link with children receive a touch event.
+// 	for (const link of linksWithChildren) {
+// 		link.addEventListener('touchstart', toggleFocus, false);
+// 	}
+
+// 	/**
+// 	 * Sets or removes .focus class on an element.
+// 	 */
+// 	function toggleFocus() {
+// 		if (event.type === 'focus' || event.type === 'blur') {
+// 			let self = this;
+// 			// Move up through the ancestors of the current link until we hit .nav-menu.
+// 			while (!self.classList.contains('nav-menu')) {
+// 				// On li elements toggle the class .focus.
+// 				if ('li' === self.tagName.toLowerCase()) {
+// 					self.classList.toggle('focus');
+// 				}
+// 				self = self.parentNode;
+// 			}
+// 		}
+
+// 		if (event.type === 'touchstart') {
+// 			const menuItem = this.parentNode;
+// 			event.preventDefault();
+// 			for (const link of menuItem.parentNode.children) {
+// 				if (menuItem !== link) {
+// 					link.classList.remove('focus');
+// 				}
+// 			}
+// 			menuItem.classList.toggle('focus');
+// 		}
+// 	}
+// }());
